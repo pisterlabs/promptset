d99addb6386082df4f8c0076974bf715d01f5330
Kaiser Pister <pisterk@gmail.com>
2023-12-02 07:37:15 -0600

    add multiprocessing

--- parse_data/find_prompts.py
+++ parse_data/find_prompts.py
@@ -1,7 +1,10 @@
 import json
+import uuid
 import os
-
-from parsers import (
+from itertools import islice
+from glob import glob
+from multiprocessing import Pool
+from parse_data.parsers import (
     PromptDetector,
     used_in_langchain_llm_call,
     used_in_openai_call,
@@ -9,23 +12,54 @@ from parsers import (
     prompt_or_template_in_name,
 )
 
-detector = PromptDetector()
+RUN_ID = f"{4:03d}"
+os.makedirs(RUN_ID, exist_ok=True)
 
-detector.add_heuristic(used_in_openai_call)
-detector.add_heuristic(used_in_langchain_llm_call)
-detector.add_heuristic(new_line_in_string)
-detector.add_heuristic(prompt_or_template_in_name)
 
-root_dir = "repos"
+def process_chunk(filenames):
+    detector = PromptDetector()
+    detector.add_heuristic(used_in_openai_call)
+    detector.add_heuristic(used_in_langchain_llm_call)
+    detector.add_heuristic(new_line_in_string)
+    detector.add_heuristic(prompt_or_template_in_name)
 
-paths = []
-for repo in os.listdir(root_dir):
-    repo_path = os.path.join(root_dir, repo)
-    for file in os.listdir(repo_path):
-        file_path = os.path.join(repo_path, file)
-        paths.append(file_path)
+    prompts = detector.detect_prompts(filenames)
+    _uuid = str(uuid.uuid4())
+    with open(f"{RUN_ID}/prompts-{_uuid}.json", "w") as w:
+        json.dump(prompts, w)
 
-results = detector.detect_prompts(paths)
 
-with open("prompts.json", "w") as file:
-    json.dump(results, file)
+def batched(iterable, n):
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError("n must be at least one")
+    it = iter(iterable)
+    while batch := tuple(islice(it, n)):
+        yield batch
+
+
+def run_all(filenames: list[str], n=32):
+    filenames_batched = batched(filenames, len(filenames) // n)
+    with Pool(n) as p:
+        p.map(process_chunk, filenames_batched)
+
+
+if __name__ == "__main__":
+    root_dir = "data/scraping/repos"
+
+    paths = []
+    for repo in os.listdir(root_dir):
+        repo_path = os.path.join(root_dir, repo)
+        for file in os.listdir(repo_path):
+            file_path = os.path.join(repo_path, file)
+            paths.append(file_path)
+
+    run_all(paths)
+
+    data = []
+    for filename in glob(f"{RUN_ID}/prompts-*.json"):
+        with open(filename) as f:
+            data.extend(json.load(f))
+
+    with open(f"final_prompts_{RUN_ID}.json", "w") as file:
+        json.dump(data, file)
