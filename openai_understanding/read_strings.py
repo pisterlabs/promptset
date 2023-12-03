17e44f1c57c9619ffaa69ca999ac01e3661decd0
Kaiser Pister <pisterk@gmail.com>
2023-12-02 21:34:52 -0600

    clean read_strings

--- openai_understanding/read_strings.py
+++ openai_understanding/read_strings.py
@@ -15,15 +15,27 @@ with open("separated-tools.json") as f:
 with open("separated-cohere.json") as f:
     data.update(json.load(f))
 
-with open("separated-name-based.json") as f:
-    data.update(json.load(f))
 
 strings = []
 for key, value in data.items():
     for string in value["strings"]:
         strings.append(string)
 
-print(len(strings))
-print(len(set(strings)))
+print("From llm calls", len(strings))
+print("Unique from llm calls", len(set(strings)))
+
+with open("strings.txt", "w") as f:
+    f.write("\n----------\n".join(strings))
+
+with open("separated-name-based.json") as f:
+    sketch_data = json.load(f)
+
+for key, value in sketch_data.items():
+    for string in value["strings"]:
+        strings.append(string)
+
+print("From llm calls + prompts", len(strings))
+print("Unique from llm calls + prompts", len(set(strings)))
+
 with open("strings_plus.txt", "w") as f:
     f.write("\n----------\n".join(strings))
