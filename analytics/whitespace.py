20955c3a960ccf5dc848e6c19afb5cb70c77e596
Kaiser Pister <pisterk@gmail.com>
2023-12-06 10:24:03 -0600

    add whitespace table

---
+++ analytics/whitespace.py
@@ -0,0 +1,55 @@
+SEP = "\n----------------------------------------------------------------------------------------\n"
+
+
+def process_strings(name, strings):
+    strings = map(lambda x: x.strip("f").strip("\"'"), strings.split(SEP))
+    strings = set(list(strings))
+
+    all_trailing_whitespace = 0
+    trailing_whitespace = 0
+    leading_whitespace = 0
+    skipped = 0
+    for prompt in strings:
+        if len(prompt) < 10:
+            skipped += 1
+            continue
+        if prompt != prompt.rstrip():
+            trailing_whitespace += 1
+        if prompt != prompt.lstrip():
+            leading_whitespace += 1
+        if prompt != prompt.strip():
+            all_trailing_whitespace += 1
+
+    l = len(strings) - skipped
+    print(
+        f"\t{name} & {l} & {trailing_whitespace} ({trailing_whitespace / l * 100:.1f}) & {leading_whitespace} ({leading_whitespace / l * 100:.1f}) & {all_trailing_whitespace} ({all_trailing_whitespace / l * 100:.1f}) \\\\"
+    )
+    # print("Analyzing whitespace for:", len(strings))
+    # print("Skipped (len < 10):", skipped)
+    # print("Trailing whitespace:", trailing_whitespace)
+    # print("Leading whitespace:", leading_whitespace)
+    # print("All trailing whitespace:", all_trailing_whitespace)
+
+
+print(
+    """\\begin{table}
+  \caption{Leading \& Trailing Whitespace Detection}
+  \label{tab:whitespace}
+  \\begin{tabular}{ccccc}"""
+)
+print("\t\\toprule")
+print("\tSet & Total & Trailing (\\%) & Leading (\\%) & All (\\%) \\\\")
+print("\t\\midrule")
+with open("strings.txt") as f:
+    strings = f.read()
+    process_strings("a, b, c, d", strings)
+
+with open("strings_plus.txt") as f:
+    strings = f.read()
+    process_strings("e", strings)
+
+print(
+    """\t\\bottomrule
+\end{tabular}
+\end{table}"""
+)
