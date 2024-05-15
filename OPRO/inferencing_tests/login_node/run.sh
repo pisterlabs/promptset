c3d74fb1f0b1b4eb451c1c4413be2aaca7d0f40f
DJ <dhrubajyotipaul3@gmail.com>
2024-05-15 01:50:34 -0500

    final config for htcondor jobs

--- OPRO/inferencing_tests/login_node/run.sh
+++ OPRO/inferencing_tests/login_node/run.sh
@@ -6,16 +6,26 @@
 # print a 'hello' message to the job's terminal output:
 echo "Hello CHTC from Job $1 running on `whoami`@`hostname`"
 
+# Setting environment variables
 export TF_ENABLE_ONEDNN_OPTS=0
+export TRANSFORMERS_CACHE=$PWD/model_cache  # .cache directory where the transformers library downloads model weights
 
-# extract model weights (removing -v flag to keep output clean)
-# tar -xzf /staging/djpaul2/gemma_model_saved.tar.gz -C ./
-
-pip3 install --upgrade pip
-pip3 install -r requirements.txt  # TODO: Use venv later
+# Moving files from staging to working directory
+if [ -f /staging/djpaul2/model_cache.tar.gz ]; then
+    # If the model_cache.tar.gz file exists, extract it
+    cp /staging/djpaul2/model_cache.tar.gz .
+    tar -xzf model_cache.tar.gz
+fi
 
 # Run Script and move output to staging
 python3 run.py > out.txt
 python3 run.py
-mv out.txt /staging/djpaul2/out.txt
+mv out.txt /staging/djpaul2/
 
+# Clean up
+if [ ! -f /staging/djpaul2/model_cache.tar.gz ]; then
+    # If the model_cache.tar.gz file doesn't exists, create and send it
+    tar -czf model_cache.tar.gz ./model_cache
+    mv model_cache.tar.gz /staging/djpaul2/
+fi
+rm -rf model_cache
