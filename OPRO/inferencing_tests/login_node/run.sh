6d35d95e5d37502fe8dff71314b058462df83308
DJ <dhrubajyotipaul3@gmail.com>
2024-05-15 03:24:45 -0500

    not compressing files to save time.

--- OPRO/inferencing_tests/login_node/run.sh
+++ OPRO/inferencing_tests/login_node/run.sh
@@ -6,6 +6,8 @@
 # print a 'hello' message to the job's terminal output:
 echo "Hello CHTC from Job $1 running on `whoami`@`hostname`"
 
+# Note: Removed -zv from tar command to save time.
+
 # Setting environment variables
 export TF_ENABLE_ONEDNN_OPTS=0
 export TRANSFORMERS_CACHE=$PWD/model_cache  # .cache directory where the transformers library downloads model weights
@@ -13,7 +15,7 @@ export TRANSFORMERS_CACHE=$PWD/model_cache  # .cache directory where the transfo
 # Moving files from staging to working directory
 if [ -f /staging/djpaul2/model_cache.tar.gz ]; then
     # If the model_cache.tar.gz file exists, extract it
-    tar -xzf /staging/djpaul2/model_cache.tar.gz -C ./
+    tar -xf /staging/djpaul2/model_cache.tar.gz -C ./
 fi
 
 # Run Script and move output to staging
@@ -23,7 +25,7 @@ mv *.json /staging/djpaul2/
 # Clean up
 if [ ! -f /staging/djpaul2/model_cache.tar.gz ]; then
     # If the model_cache.tar.gz file doesn't exists, create and send it
-    tar -czf model_cache.tar.gz ./model_cache
+    tar -cf model_cache.tar.gz ./model_cache
     mv model_cache.tar.gz /staging/djpaul2/
 fi
 rm -rf model_cache
